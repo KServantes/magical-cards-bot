6122c67127c667b540904b4df5d356812b9b20c1
KServantes <elitalianoverde@gmail.com>
2022-07-19 03:08:43 -0600

    db schema edit

--- data/migrations/20220706132421_members.js
+++ data/migrations/20220706132421_members.js
@@ -9,6 +9,16 @@ exports.up = async knex => {
 			cards.integer('id').primary();
 			cards.string('name').notNullable();
 			cards.string('desc');
+			cards.integer('ot').defaultTo(32);
+			cards.integer('alias').defaultTo(0);
+			cards.integer('setcode').defaultTo(0);
+			cards.integer('type').defaultTo(0);
+			cards.integer('atk').defaultTo(0);
+			cards.integer('def').defaultTo(0);
+			cards.integer('level').defaultTo(0);
+			cards.integer('race').defaultTo(0);
+			cards.integer('attribute').defaultTo(0);
+			cards.integer('category').defaultTo(0);
 		});
 	await knex.schema
 		.createTable('member_cards', memcards => {
