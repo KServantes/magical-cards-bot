3ccdcd09691354110a137c333facfcb888c5e977
KServantes <elitalianoverde@gmail.com>
2022-10-03 04:57:39 -0600

    library remake - add cache to keep track of member stuff - change command interaction to embed with buttons

--- commands/library.js
+++ commands/library.js
@@ -1,8 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
 const Cards = require('../data/models');
+const Cache = require('../commands/library/cache');
 const wait = require('node:timers/promises').setTimeout;
-const Helper = require('./library/msgHelper');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -20,35 +20,88 @@ module.exports = {
 				.setAutocomplete(true),
 		),
 	async execute(interaction) {
-		const userOp = interaction.options.getUser('duelist');
-		const { id: memberID } = userOp ? userOp : interaction.member;
+		const { member, client, options, user } = interaction;
+		// const userOp = options.getUser('duelist');
+		// const { id: memberID } = userOp ? userOp : member;
 		const cards = await Cards.getAllCards();
-		const cardCount = cards.length;
 		const servers = await Cards.getAllServers();
+
+		const cardCount = cards.length;
 		const servCount = servers.length;
+		const maxPage = Math.ceil(cards.length / 10);
+
+		const { cache } = client;
+		const memInfo = Cache.getMemberInfo(cache, member);
+		const { page, select } = memInfo;
+
+		if (memInfo && page !== 1) {
+			const end = page * 10;
+			const start = end / 10;
+			const pageCards = cards.slice(start, end);
+			memInfo.pageInfo = pageCards;
+		}
+		else {
+			const fpage = cards.slice(0,10);
+			memInfo.pageInfo = fpage;
+		}
 
 		if (cardCount >= 1) {
 			try {
 				const extraS = count => count > 1 ? 's' : '';
 				const msg = `I have a total of ${cardCount} card${extraS(cardCount)} in ${servCount} server${extraS(servCount)}.`;
-				const topEmbed = new MessageEmbed()
-					.setColor('#0099ff')
+
+				const descStrings = memInfo.pageInfo.reduce((acc, card) => {
+					const stringAs = `[${card.id}] - ${card.name} `;
+					return acc.concat(stringAs);
+				}, []);
+
+				const desc = descStrings.reduce((acc, str, i) => {
+					let format = `[${i + 1}] | ` + str;
+					if ((i + 1) === select) {
+						format = `**${format}**`;
+					}
+
+					return acc + `${format} \n`;
+				}, '');
+
+
+				const url = user.displayAvatarURL();
+				const cardsEmbed = new MessageEmbed()
+					.setColor('#7ec460')
 					.setTitle('Library')
-					.setDescription(msg)
-					.setThumbnail('https://i.imgur.com/ebtLbkK.png');
+					.setDescription('>>> ' + msg + '\n\n' + desc)
+					.setThumbnail('https://i.imgur.com/ebtLbkK.png')
+					.setFooter({ text: `Page 1 of ${maxPage}`, iconURL: url });
 
-				return await interaction.reply({ embeds: [topEmbed] });
-				// const msg = Helper.initialMsg(cards);
-				// const reply = await interaction.reply({ content: msg, fetchReply: true });
-				// Helper.reactToMsg(reply, cards);
 
-				// const collection = await Helper.reactionCollection(reply);
-				// if (collection.size < 1) return await reply.reactions.removeAll();
-				// const reaction = collection.first();
-				// const { name } = reaction.emoji;
-				// const card = Helper.getCardFromReact(name, cards);
-				// const msgContent = `Library | Cards: ${cards.length}\n[${card.id}]\n${card.name}\n${card.desc}\n`;
-				// Helper.msgLoop(reply, { content: Helper.formatMsg(msgContent) }, cards);
+				const prevPage = new MessageButton()
+					.setCustomId('lib prev page')
+					.setLabel('<<')
+					.setDisabled(page === 1 ? true : false)
+					.setStyle('PRIMARY');
+				const nextPage = new MessageButton()
+					.setCustomId('lib next page')
+					.setLabel('>>')
+					.setDisabled(page === maxPage ? true : false)
+					.setStyle('PRIMARY');
+
+
+				const row = new MessageActionRow().addComponents(prevPage, nextPage);
+
+				const buttons = new Array(10);
+				for (const index of buttons.keys()) {
+					buttons[index] = new MessageButton()
+						.setCustomId(`card ${index}`)
+						.setLabel(`${ 1 + index }`)
+						.setStyle('SECONDARY');
+				}
+
+				const btop = buttons.slice(0, 5);
+				const bsec = buttons.slice(5);
+				const srow1 = new MessageActionRow().addComponents(btop);
+				const srow2 = new MessageActionRow().addComponents(bsec);
+
+				return await interaction.reply({ embeds: [cardsEmbed], components: [row, srow1, srow2] });
 			}
 			catch (err) {
 				console.log({ err });
---
+++ commands/library/cache.js
@@ -0,0 +1,49 @@
+const { Collection } = require('discord.js');
+
+const CACHE_LIBRARY = 'library cache';
+
+const getLibraryCache = cache => {
+	const libCache = cache.get(CACHE_LIBRARY);
+	if (!libCache) {
+		const Library = new Collection();
+
+		const newCache = cache.set(CACHE_LIBRARY, Library);
+		return newCache;
+	}
+
+	return libCache;
+};
+
+const setMemberInfo = (cache, member) => {
+	const memberInfo = {
+		page: 1,
+		pageInfo: [],
+		select: 0,
+		details: false,
+	};
+
+	const libraryCache = getLibraryCache(cache, member);
+
+	const memberColl = libraryCache.set(member.id, memberInfo);
+	const memInfo = memberColl.get(member.id);
+
+	return memInfo;
+};
+
+const getMemberInfo = (cache, member) => {
+	const libraryCache = getLibraryCache(cache);
+	const info = libraryCache.get(member.id);
+
+	if (!info) {
+		const newInfo = setMemberInfo(cache, member);
+
+		return newInfo;
+	}
+
+	return info;
+};
+
+module.exports = {
+	getLibraryCache,
+	getMemberInfo,
+};
