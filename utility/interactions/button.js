aef70e795bf595515758bf15e57b407b67042849
Keddy <elitalianoverde@gmail.com>
2025-01-11 17:40:51 -0700

    add button handler

---
+++ utility/interactions/button.js
@@ -0,0 +1,23 @@
+const Create = require('commands/create');
+
+const isButton = interaction => {
+	return interaction.isButton();
+};
+
+const interactionButton = async (interaction) => {
+
+	const { customId } = interaction;
+
+	Create.button.forEach((fun, key) => {
+		if (customId === key) {
+			fun(interaction);
+		}
+	});
+
+};
+
+module.exports = {
+	name: 'button',
+	type: isButton,
+	interact: interactionButton,
+};
